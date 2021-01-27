b4fa8c3b7d89229530a697d46499ebce556442a3
Annastar395 <77985478+Annastar395@users.noreply.github.com>
2021-01-27 12:41:02 +0000

    my net

--- Lesson_4.py
+++ Lesson_4.py
@@ -1,1 +1,19 @@
-print("hello, world")
+my_tuple = (1,2,3, "twenty", True, (1,2),"twenty")
+print(type(my_tuple))
+
+print(my_tuple)
+print(my_tuple[::-2])
+print(my_tuple[-1][-1])
+for value in my_tuple:
+    print(value)
+    my_tuple.index(3)
+print(my_tuple.index("twenty",6))
+print(my_tuple.count(8))
+a=2
+b=3
+# tmp=a
+# a=b
+# b=tmp
+a,b = b, a
+print(a,b)
+
